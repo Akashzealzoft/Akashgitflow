100e0c66bb5bce5a0825478529ab82c1b86d37ba
Akashzealzoft <akash@zealzoft.com>
2023-12-29 13:13:23 +0530

    my work here is done

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div>
+        Header main color
+        <div>
+          <p>Dell is my laptop - Akash i made this change</p>
+          <p>santhana kumar</p>
+        </div>
+      </div>
+    </>
+  );
 }
