009835224f9fba514158ecd14dcaf4e0d11767ce
KarthikRaja <karthikrajaveluchamy2001@gmail.com>
2023-12-29 13:13:47 +0530

    kumar is edited

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div>Header main color
+      <div>
+        <p>AKash</p>
+        <p>Edited by kumar</p>
+      </div>
+    </div>
+    </>
+  );
 }
